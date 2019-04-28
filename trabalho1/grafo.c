8e12ad46b8930f69ae2d58ec8bf509118ddd06df
Lucas Rosa <luc1604@hotmail.com>
2019-04-28 12:42:23 -0300

    inicio de exclusao de arvore
    Signed-off-by: Lucas Rosa <luc1604@hotmail.com>

--- trabalho1/grafo.c
+++ trabalho1/grafo.c
@@ -188,117 +188,117 @@ int checa_grafo(grafo *g){
   return cont;
 }
 
-no *cria_no(vertice *v){
-  no *n = (no *)malloc(sizeof(no));
-  assert(n);
-  n->conteudo = v;
-  n->filhos = cria_lista();
-  n->nivel = -1;
-  return n;
-}
-
-void destroi_no(no *n){
-  for(item *aux=n->filhos->head; aux!=NULL; aux=aux->prox){
-    destroi_no(aux->conteudo);
-  }
-  destroi_lista(n->filhos);
-  free(n);
-}
-
-arvore* cria_arvore(){
-  arvore *t = (arvore *)malloc(sizeof(arvore));
-  assert(t);
-  t->raiz = NULL;
-  t->altura = 0;
-  return t;
-}
-
-void destroi_arvore(arvore *t){
-  if(t!=NULL){
-    destroi_no(t->raiz);
-    free(t);
-  }
-}
-
-int adiciona_no(arvore *t, no *pai, no *filho){
-  if(pai == NULL){
-    t->raiz = filho;
-    filho->nivel = 0;
-  }else if(filho->nivel == -1 || pai->nivel < filho->nivel -1){
-    adiciona_item(pai->filhos, (void *)filho);
-    filho->nivel = pai->nivel + 1;
-  }else{
-    // adicao de um filho ja inserido na arvore nao sera implementada
-    puts("UNEXPECTED");
-  }
-  return filho->nivel;
-}
-
-no* acha_no(arvore *t, vertice *v){
-  lista *l = cria_lista();
-  adiciona_item(l, (void *)t->raiz);
-  for(item *aux = l->head; aux!=NULL; aux = l->head){
-    if(((vertice *)((no *)aux->conteudo)->conteudo) == v){
-      no *n = (no *)aux->conteudo;
-      destroi_lista(l);
-      return n;
-    }
-    for(item *x = ((no *)aux->conteudo)->filhos->head; x!=NULL; x = x->prox){
-      adiciona_item(l, (no *)x->conteudo);
-    }
-    remove_item(l, l->head->conteudo);
-  }
-  destroi_lista(l);
-  return NULL;
-}
-
-arvore* gera_arvore(vertice *v){
-  int altura;
-  item *aux, *vaux;
-  arvore *t = cria_arvore();
-  lista *l = cria_lista();
-  t->altura = 1 + adiciona_no(t, NULL, cria_no(v));
-  adiciona_item(l, (void *)v);
-  for(aux = l->head; aux!=NULL; aux = aux->prox){
-    for(vaux = ((vertice *)aux->conteudo)->vizinhos->head; vaux!=NULL; vaux = vaux->prox){
-      if(acha_item(l, (void *)vaux->conteudo)==NULL){
-        adiciona_item(l, (void *)vaux->conteudo);
-        no *a = acha_no(t, (vertice *)aux->conteudo);
-        no *b = cria_no((vertice *)vaux->conteudo);
-        altura = 1 + adiciona_no(t, a, b);
-        if(altura > t->altura){
-          t->altura = altura;
-        }
-      }
-    }
-  }
-  destroi_lista(l);
-  return t;
-}
-
-int checa_arvore(no *n){
-  int soma = n->conteudo->tam;
-  for(item *aux = n->filhos->head; aux != NULL; aux = aux->prox){
-    soma += checa_arvore((no *)aux->conteudo);
-  }
-  return soma;
-}
-
-arvore* encontra_melhor_arvore(grafo *g){
-  item *aux;
-  arvore *best=NULL, *taux;
-  int tam_best = MAX_INT;
-  for(aux = g->vertices->head; aux!=NULL; aux = aux->prox){
-    taux = gera_arvore((vertice *)aux->conteudo);
-    printf("%d\n", taux->altura); // DEBUG
-    if(taux->altura < tam_best){
-      destroi_arvore(best);
-      best = taux;
-      tam_best = best->altura;
-    }
-  }
-  return best;
-}
+// no *cria_no(vertice *v){
+//   no *n = (no *)malloc(sizeof(no));
+//   assert(n);
+//   n->conteudo = v;
+//   n->filhos = cria_lista();
+//   n->nivel = -1;
+//   return n;
+// }
+//
+// void destroi_no(no *n){
+//   for(item *aux=n->filhos->head; aux!=NULL; aux=aux->prox){
+//     destroi_no(aux->conteudo);
+//   }
+//   destroi_lista(n->filhos);
+//   free(n);
+// }
+//
+// arvore* cria_arvore(){
+//   arvore *t = (arvore *)malloc(sizeof(arvore));
+//   assert(t);
+//   t->raiz = NULL;
+//   t->altura = 0;
+//   return t;
+// }
+//
+// void destroi_arvore(arvore *t){
+//   if(t!=NULL){
+//     destroi_no(t->raiz);
+//     free(t);
+//   }
+// }
+//
+// int adiciona_no(arvore *t, no *pai, no *filho){
+//   if(pai == NULL){
+//     t->raiz = filho;
+//     filho->nivel = 0;
+//   }else if(filho->nivel == -1 || pai->nivel < filho->nivel -1){
+//     adiciona_item(pai->filhos, (void *)filho);
+//     filho->nivel = pai->nivel + 1;
+//   }else{
+//     // adicao de um filho ja inserido na arvore nao sera implementada
+//     puts("UNEXPECTED");
+//   }
+//   return filho->nivel;
+// }
+//
+// no* acha_no(arvore *t, vertice *v){
+//   lista *l = cria_lista();
+//   adiciona_item(l, (void *)t->raiz);
+//   for(item *aux = l->head; aux!=NULL; aux = l->head){
+//     if(((vertice *)((no *)aux->conteudo)->conteudo) == v){
+//       no *n = (no *)aux->conteudo;
+//       destroi_lista(l);
+//       return n;
+//     }
+//     for(item *x = ((no *)aux->conteudo)->filhos->head; x!=NULL; x = x->prox){
+//       adiciona_item(l, (no *)x->conteudo);
+//     }
+//     remove_item(l, l->head->conteudo);
+//   }
+//   destroi_lista(l);
+//   return NULL;
+// }
+//
+// arvore* gera_arvore(vertice *v){
+//   int altura;
+//   item *aux, *vaux;
+//   arvore *t = cria_arvore();
+//   lista *l = cria_lista();
+//   t->altura = 1 + adiciona_no(t, NULL, cria_no(v));
+//   adiciona_item(l, (void *)v);
+//   for(aux = l->head; aux!=NULL; aux = aux->prox){
+//     for(vaux = ((vertice *)aux->conteudo)->vizinhos->head; vaux!=NULL; vaux = vaux->prox){
+//       if(acha_item(l, (void *)vaux->conteudo)==NULL){
+//         adiciona_item(l, (void *)vaux->conteudo);
+//         no *a = acha_no(t, (vertice *)aux->conteudo);
+//         no *b = cria_no((vertice *)vaux->conteudo);
+//         altura = 1 + adiciona_no(t, a, b);
+//         if(altura > t->altura){
+//           t->altura = altura;
+//         }
+//       }
+//     }
+//   }
+//   destroi_lista(l);
+//   return t;
+// }
+//
+// int checa_arvore(no *n){
+//   int soma = n->conteudo->tam;
+//   for(item *aux = n->filhos->head; aux != NULL; aux = aux->prox){
+//     soma += checa_arvore((no *)aux->conteudo);
+//   }
+//   return soma;
+// }
+//
+// arvore* encontra_melhor_arvore(grafo *g){
+//   item *aux;
+//   arvore *best=NULL, *taux;
+//   int tam_best = MAX_INT;
+//   for(aux = g->vertices->head; aux!=NULL; aux = aux->prox){
+//     taux = gera_arvore((vertice *)aux->conteudo);
+//     printf("%d\n", taux->altura); // DEBUG
+//     if(taux->altura < tam_best){
+//       destroi_arvore(best);
+//       best = taux;
+//       tam_best = best->altura;
+//     }
+//   }
+//   return best;
+// }
 
 vertice* encontra_melhor_vertice(grafo *g){
   int tamanho = 0;
@@ -312,11 +312,11 @@ vertice* encontra_melhor_vertice(grafo *g){
   return v;
 }
 
-jogo* cria_jogo(arvore *t, int tam){
+jogo* cria_jogo(vertice *v, int tam){
   jogo *j = (jogo *)malloc(sizeof(jogo));
   assert(j);
-  j->x = t->raiz->conteudo->coord_x;
-  j->y = t->raiz->conteudo->coord_y;
+  j->x = v->coord_x;
+  j->y = v->coord_y;
   j->n_jogadas = 0;
   j->jogadas = (int *)malloc(sizeof(int)*tam);
   assert(j->jogadas);
@@ -337,46 +337,71 @@ int encontra_melhor_cor(no *r){
   return melhor+1;
 }
 
-void pinta(arvore *t, int cor){
-  no *b;
-  item *aux = t->raiz->filhos->head;
-  t->raiz->conteudo->cor = cor;
-  while(aux!=NULL){
-    b = ((no *)aux->conteudo);
-    aux = aux->prox;
-    if(b->conteudo->cor == cor){
-      while(b->filhos->tam != 0){
-        adiciona_no(t, t->raiz, (no *)b->filhos->head->conteudo);
-        remove_item(b->filhos, b->filhos->head->conteudo);
-      }
-      t->raiz->conteudo->tam += b->conteudo->tam;
-      remove_item(t->raiz->filhos, (void *)b);
-      destroi_no(b);
-    }
-  }
-}
-
-jogo* floodit(arvore *t){
-  int tam_max = 8, cor;
-  jogo *j = cria_jogo(t, tam_max);
-  while(t->raiz->filhos->tam > 0){
-    cor = encontra_melhor_cor(t->raiz);
-    pinta(t, cor);
-    if(j->n_jogadas >= tam_max){
-      tam_max = tam_max*2;
-      j->jogadas = (int *)realloc(j->jogadas, sizeof(int)*tam_max);
-      assert(j->jogadas);
-    }
-    j->jogadas[j->n_jogadas] = cor;
-    j->n_jogadas++;
-  }
-  return j;
-}
+// jogo* cria_jogo(arvore *t, int tam){
+//   jogo *j = (jogo *)malloc(sizeof(jogo));
+//   assert(j);
+//   j->x = t->raiz->conteudo->coord_x;
+//   j->y = t->raiz->conteudo->coord_y;
+//   j->n_jogadas = 0;
+//   j->jogadas = (int *)malloc(sizeof(int)*tam);
+//   assert(j->jogadas);
+//   return j;
+// }
+//
+// int encontra_melhor_cor(no *r){
+//   int vizinhos[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//   for(item *aux = r->filhos->head; aux!=NULL; aux = aux->prox){
+//     vizinhos[((no *)aux->conteudo)->conteudo->cor-1] += ((no *)aux->conteudo)->filhos->tam + 1;
+//   }
+//   int melhor = 0;
+//   for(int i=1; i<10; i++){
+//     if(vizinhos[melhor] < vizinhos[i]){
+//       melhor = i;
+//     }
+//   }
+//   return melhor+1;
+// }
+//
+// void pinta(arvore *t, int cor){
+//   no *b;
+//   item *aux = t->raiz->filhos->head;
+//   t->raiz->conteudo->cor = cor;
+//   while(aux!=NULL){
+//     b = ((no *)aux->conteudo);
+//     aux = aux->prox;
+//     if(b->conteudo->cor == cor){
+//       while(b->filhos->tam != 0){
+//         adiciona_no(t, t->raiz, (no *)b->filhos->head->conteudo);
+//         remove_item(b->filhos, b->filhos->head->conteudo);
+//       }
+//       t->raiz->conteudo->tam += b->conteudo->tam;
+//       remove_item(t->raiz->filhos, (void *)b);
+//       destroi_no(b);
+//     }
+//   }
+// }
+//
+// jogo* floodit(arvore *t){
+//   int tam_max = 8, cor;
+//   jogo *j = cria_jogo(t, tam_max);
+//   while(t->raiz->filhos->tam > 0){
+//     cor = encontra_melhor_cor(t->raiz);
+//     pinta(t, cor);
+//     if(j->n_jogadas >= tam_max){
+//       tam_max = tam_max*2;
+//       j->jogadas = (int *)realloc(j->jogadas, sizeof(int)*tam_max);
+//       assert(j->jogadas);
+//     }
+//     j->jogadas[j->n_jogadas] = cor;
+//     j->n_jogadas++;
+//   }
+//   return j;
+// }
 
 void imprime_solucao(jogo *j){
   printf("%d %d %d\n", j->x, j->y, j->n_jogadas);
   for (int i = 0; i < j->n_jogadas; i++) {
     printf("%d ", j->jogadas[i]);
   }
-  printf("\n");
+  puts("");
 }
--- trabalho1/run.sh
+++ trabalho1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gcc grafo.c map.c main.c -o teste -O3
-./geramapa 100 100 10 > tmp.map
+./geramapa 10 10 3 > tmp.map
 # time ./teste < tmp.map
 time ./teste < tmp.map > solucao.txt
 cat tmp.map solucao.txt | ./anima

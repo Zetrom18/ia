26cddc19615d1e304cbfea996887d38b318ba7b3
Lucas <lucas.rosa.simepar@gmail.com>
2019-06-07 11:40:55 -0300

    renamed trab1, added catapultas
    Signed-off-by: Lucas <lucas.rosa.simepar@gmail.com>

--- trabalho1/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcc grafo.c map.c main.c -o teste
-# ./geramapa 100 100 10 > tmp.map
-# time ./teste < tmp.map
-time ./teste < mapa_exemplo_100_100_10.txt
-# cat tmp.map solucao.txt | ./anima
